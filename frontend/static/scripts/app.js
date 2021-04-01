9d32d573cd2300ce52a92682ff1c3afca7df0667
Cooper Strahan <cooperstrahan@gmail.com>
2021-03-31 21:13:27 -0600

    more front end stuff

--- frontend/static/scripts/app.js
+++ frontend/static/scripts/app.js
@@ -1,14 +1,44 @@
+// import Vue from 'vue'
+
 Vue.createApp({
   data() {
     return {
-      candidates: [],
-      enteredValue: ''
+
     };
   },
   methods: {
-    addCandidate() {
-      this.candidates.push(this.enteredValue);
-      this.enteredValue = '';
+    registerVoter() {
+      
     }
   }
-}).mount('#app');
+}).mount("#app-1");
+
+
+Vue.createApp({
+  data() {
+    return {
+      selected: '',
+      options: [
+        { candidate: 'John', value: 'John'},
+        { candidate: 'Jacob', value: 'Jacob'},
+        { candidate: 'Jeremy', value: 'Jeremy'},
+        { candidate: 'Jedediah', value: 'Jedediah'},
+      ]
+    }
+  },
+  methods: {
+
+  }
+}).mount("#app-2");
+
+
+Vue.createApp({
+  data() {
+    return {
+
+    };
+  },
+  methods: {
+
+  }
+}).mount("#app-3");
