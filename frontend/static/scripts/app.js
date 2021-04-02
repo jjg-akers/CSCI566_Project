a4d528097a9cc855fd134155235a59d4a1778204
Cooper Strahan <cooperstrahan@gmail.com>
2021-04-02 15:03:38 -0600

    hitting registrar endpoint correctly

--- frontend/static/scripts/app.js
+++ frontend/static/scripts/app.js
@@ -3,12 +3,25 @@
 Vue.createApp({
   data() {
     return {
-
+      fname: '',
+      lname: '',
+      idnumber: ''
     };
   },
   methods: {
     registerVoter() {
+      const unique_hash = '';
+      axios
+        .post('http://localhost:8040/registrar' )
+          .then(response => this.unique_hash = response.data)
+          .catch(error => {
+            this.errorMessage = error.message;
+            console.error("There was an error!", error);
+          });
       
+          this.fname='';
+          this.lname='';
+          this.idnumber='';
     }
   }
 }).mount("#app-1");
@@ -18,16 +31,26 @@ Vue.createApp({
   data() {
     return {
       selected: '',
+      hashvalue: '',
       options: [
-        { candidate: 'John', value: 'John'},
-        { candidate: 'Jacob', value: 'Jacob'},
-        { candidate: 'Jeremy', value: 'Jeremy'},
-        { candidate: 'Jedediah', value: 'Jedediah'},
-      ]
+        { candidate: 'John', value: 'John', id: 1 },
+        { candidate: 'Jacob', value: 'Jacob', id: 2},
+        { candidate: 'Jeremy', value: 'Jeremy', id: 3},
+        { candidate: 'Jedediah', value: 'Jedediah', id: 4},
+      ],
     }
   },
   methods: {
+    async submitVote() {
 
+      const response = await axios.get("http://localhost:8040/vote/", this.hashvalue)
+      
+      //TODO: rewrite the POST request
+      if (response == true){
+        axios.post("http://localhost:8040/election_guard/vote/", this.option.id);
+      }
+
+    }
   }
 }).mount("#app-2");
 
@@ -35,10 +58,13 @@ Vue.createApp({
 Vue.createApp({
   data() {
     return {
-
+      verificationcode: '',
     };
   },
   methods: {
+    async verify() {
+      const response = await axios.get("http://localhost:8040/voters/", this.verificationcode)
+    }
 
   }
 }).mount("#app-3");
