434075ed4615e5d8d7bfa0b8caad39efda530758
Joseph-Akers <jjg.akers@gmail.com>
2021-04-07 21:07:03 -0600

    Parse voter

--- api/handlers/voter_registration.go
+++ api/handlers/voter_registration.go
@@ -1,15 +1,23 @@
 package handlers
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 
+	"github.com/gorilla/schema"
 	"github.com/jjg-akers/csci556_project/registrar"
 )
 
 type VoterRegistrationHandler struct {
 	registrar.Registrar
+}
+
+type Voter struct{
+	First_name string `schema:"first_name"`
+	Last_name string `schema:"last_name"`
+	Idnumber string `schema:"idnumber"`
 }
 
 func (h *VoterRegistrationHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -23,12 +31,36 @@ func (h *VoterRegistrationHandler) ServeHTTP(w http.ResponseWriter, r *http.Requ
 		log.Println(r.Method)
 		log.Println(r.URL.Query().Get("verificationcode"))
 	case http.MethodPost:
-		log.Println(r.Method)
-		log.Println(r.Body)
-		// log.Println(r.ParseForm())
-		// log.Println(r.URL.Query().Post("fname"))
-		// log.Println(r.URL.Query().Post("lname"))
-		// log.Println(r.URL.Query().Post("idnumber"))
+		// log.Println(r.Method)
+		// log.Println(r.Body)
+		d := schema.NewDecoder()
+		log.Println("processing post")
+
+		err := r.ParseForm()
+		if err != nil{
+			log.Println("parse form failed")
+			w.WriteHeader(http.StatusBadRequest)
+			return
+	
+		}
+	
+		voter := &Voter{}
+
+		if err = d.Decode(voter, r.PostForm); err != nil {
+			log.Println("err decoding post form: ", err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		v, err := json.MarshalIndent(voter, "", "  ")
+		if err != nil{
+			log.Println("err marshalling")
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		log.Println("voter: ", string(v))
+
 	case http.MethodPut:
 		// Update an existing record.
 	case http.MethodDelete:
