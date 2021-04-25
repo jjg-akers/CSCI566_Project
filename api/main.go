e5086979dc09a8220ccc3cf7a44f4d787afd80e2
Joseph-Akers <jjg.akers@gmail.com>
2021-04-24 22:26:23 -0600

    Start ganache testnet with preloaded eth Deploy base contract on start up

--- api/main.go
+++ api/main.go
@@ -8,9 +8,14 @@ import (
 	"os"
 	"os/signal"
 	"sync"
+	"time"
 
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/jjg-akers/csci556_project/build"
 	"github.com/jjg-akers/csci556_project/handlers"
+	"github.com/jjg-akers/csci556_project/registrar"
+	"github.com/joho/godotenv"
 	"github.com/urfave/cli/v2"
 )
 
@@ -24,7 +29,24 @@ func newVotingApp() *votingApp {
 	}
 }
 
+const envLoc = "./../.env"
+
+
+func loadEnv() error{
+	var err error
+	if err = godotenv.Load(envLoc); err != nil {
+		log.Printf("could not load env from %s: %v", envLoc, err)
+		return err
+	}
+	return nil
+}
+
 func main() {
+	// time.Sleep(time.Second*10)
+
+	// if err := loadEnv(); err != nil{
+	// 	log.Fatalf("failed to load env: %s", err)
+	// }
 
 	api := newVotingApp()
 
@@ -52,6 +74,32 @@ func (api *votingApp) startAPI(cliCtx *cli.Context) error {
 	}
 
 	defer registrar.Client.Close()
+
+	// try to deploy contract
+	proceed := false
+	var proceedErr error
+	for i:= 0; i< 3; i ++ {
+		if proceed{
+			break
+		}
+
+		time.Sleep(time.Second*time.Duration((i*i)))
+		if proceedErr = initContract(cliCtx.Context, registrar.Client); proceedErr != nil{
+			proceedErr = fmt.Errorf("failed to initialize base contract: %w", proceedErr)
+			continue
+		}
+
+		proceed = true
+	}
+
+	if proceedErr != nil{
+		return proceedErr
+	}
+
+	accountAddress := common.HexToAddress("2e4adcdc087cc47d7940f14009a56747d7244afb")
+	// common: 0x7809169cf6772832f6c80e5334E6DB9e366fBb2d
+    balance, _ := registrar.Client.BalanceAt(cliCtx.Context, accountAddress, nil)
+    log.Printf("Application wallet Eth Balance: %d\n",balance)
 
 	registrationHandler := &handlers.VoterRegistrationHandler{
 		Registrar: registrar,
@@ -82,4 +130,34 @@ func startServer(ctx context.Context, wg *sync.WaitGroup, interrupt chan os.Sign
 
 	<-interrupt
 	wg.Done()
+}
+
+//CONTRACTADDR="0x56c43E8b7441Fc1833c750CE2430D8Ac4617aDac"
+
+func initContract(ctx context.Context, client *ethclient.Client) ( error) {
+	// create a new session
+	// We havn't spedified a value for the contract field yet, we will do that after obtaining a contract instance
+	// when we deploy a new contract or whaen we load an existing
+	var err error
+	
+	session, err := registrar.NewSession(ctx)
+	if err != nil{
+		return fmt.Errorf("error getting new session: %w", err)
+	}
+
+	// deploy if not exist
+		//load or deploy contract, and update session with contract instance
+		
+		if addr := os.Getenv("CONTRACTADDR"); addr == ""{
+			_, err = registrar.NewContract(session, client)
+			if err != nil{
+				return err
+			}
+			log.Println("new contract deployed successfully")
+			return nil
+		} 
+
+		log.Println("contract address already exists at: ", os.Getenv("CONTRACTADDR"))
+
+		return nil
 }
--- api/registrar/registrar.go
+++ api/registrar/registrar.go
@@ -1,7 +1,113 @@
 package registrar
 
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/jjg-akers/csci556_project/contract"
+
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
 // Registrar defines the methods necessary for a registrar
 type Registrar interface{
 	RegisterVoter([]byte) error
 	CheckRegistration([]byte) (bool, error)
+}
+
+//Sessions are wrappers that allow us to make contract calls without having to pass aorund auth, creds,
+//  and call parameters constantly
+//A session wraps:
+//  a contract instance
+//	a bind.CallOpts struct that contains opts for making contract calls
+//	a bind.TransactOpts struct that contains auth creds and params for created a valid Ethereum transaction
+func NewSession(ctx context.Context) (*contract.ContractSession, error) {
+
+	// read data from keystore file
+	keystore, err := os.Open(os.Getenv("KEYSTORE"))
+	// keystore, err := os.Open(keystorePath)
+	if err != nil {
+		return nil, fmt.Errorf("could not load keystore: %w", err)
+	}
+
+	defer keystore.Close()
+
+	keystorepass := os.Getenv("KEYSTORE_PASS")
+	if keystorepass == ""{
+		log.Println("key store pass is empty")
+	}
+
+	// get transact opts
+	auth, err := bind.NewTransactor(keystore, keystorepass)
+	if err != nil {
+		return nil, fmt.Errorf("error getting auth: %w", err)
+	}
+
+	callOpts := bind.CallOpts{
+		From:    auth.From,
+		Context: ctx,
+	}
+
+	//return session without contract instance - we'll do that after we've obtained a contract instance when we deploy a new contract
+	//  or when we load an existing contract
+	return &contract.ContractSession{
+		TransactOpts: *auth,
+		CallOpts:     callOpts,
+	}, nil
+}
+
+//NewContract deploys a contract if no existing contract exists and assigns to session
+//A new contract takes the following params
+//session: the client object, which we initialezed in main()
+//question: a string containing the question we want the user to answer
+//answer: the answer to the question
+func NewContract(session *contract.ContractSession, client *ethclient.Client) (*contract.ContractSession, error) {
+	log.Println("Creating new contract")
+
+	// deploy quizt to get the contract addr, transaction object, adnd instance
+	contractAddress, tx, instance, err := contract.DeployContract(&session.TransactOpts, client)
+	if err != nil {
+		return nil, fmt.Errorf("could not deploy contract: %w", err)
+	}
+
+	//print the adress of the transaction - this can be used to up on ethresan the progress of the transaction
+	fmt.Println("Contract deployed! Wait for tx %s to be confirmed.\n", tx.Hash().Hex())
+
+	session.Contract = instance
+
+	//save the address of the deployed contract
+	// updateEnvFile("CONTRACTADDR", contractAddress.Hex())
+	if err := os.Setenv("CONTRACTADDR", contractAddress.Hex()); err != nil{
+		return nil, err
+	}
+
+	return session, nil
+}
+
+//LoadContract loads a contract if one exists, and assigns to the session
+//attempts to load an existing contract by looking for a entry in the env file
+func LoadContract(session contract.ContractSession, client *ethclient.Client, contractAddr string) (*contract.ContractSession, error) {
+	log.Println("Loading contract")
+
+	// check for existing contract
+	// addr := common.HexToAddress(os.Getenv("CONTRACTADDR"))
+	addr := common.HexToAddress(contractAddr)
+
+
+	// create new contract instance
+	instance, err := contract.NewContract(addr, client) //creates a new instance of Quiz, bound to a specific deployed contract
+	if err != nil {
+		// if addr doesn't exists we don't know where to locate our contract on the blockchain
+		// log.Fatalf("could not load contract: %v\n", err)
+		//log.Println(ErrTransactionWait)
+		return nil, fmt.Errorf("could not load contract: %w", err)
+	}
+
+	session.Contract = instance
+
+	return &session, nil
 }
